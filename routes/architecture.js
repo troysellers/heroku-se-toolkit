110e0d1fd5b2f99a754a1796db1b3f056cc1dd61
tsellers <tsellers@salesforce.com>
2017-06-19 15:59:03 +1000

    images

--- routes/architecture.js
+++ routes/architecture.js
@@ -2,13 +2,15 @@
 
 var express = require('express');
 var router = express.Router();
+var jsonfile = require('jsonfile');
+var file = './data/architectures.json';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
    req.data.title = 'Common Architectures';
    req.data.architecture_style = 'slds-is-active';
-   
+   req.data.architectures = jsonfile.readFileSync(file);
 
    res.render('architecture', req.data);
 });
