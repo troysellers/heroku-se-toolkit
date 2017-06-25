476fcbf977f1ad7b38ca4ca4a0538dea4150343b
tsellers <tsellers@salesforce.com>
2017-06-26 08:03:01 +1000

    fix

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ app.use(session({
    })
 }));
 */
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
