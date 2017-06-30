0ae1626699b8383172bee73ea154402ca3883e46
tsellers <tsellers@salesforce.com>
2017-06-30 15:29:07 +1000

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,10 @@
 
 var express = require('express');
 var router = express.Router();
-var logger = require('morgan');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-   logger.info('Home page...');
    req.data.title = 'Why Heroku?';
    req.data.why_style = 'slds-is-active';
    console.log(req.data.resources);
