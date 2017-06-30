68d9a42eabd2bde92e726d0a67dd96363cc6ccc5
tsellers <tsellers@salesforce.com>
2017-06-30 14:51:19 +1000

    fix

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
-var expressLogging = require('express-logging');
-var logger = require('logops');
 /*
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
@@ -30,7 +28,6 @@ var customers = require('./routes/customers');
 var resourceFilter = require('./bin/resourceFilter');
 
 var app = express();
-app.use(expressLogging(logger));
 
 let redis_url = process.env.REDIS_URL ? process.env.REDIS_URL : 'redis://localhost:6379'
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var router = express.Router();
-var logger = require('logops');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
