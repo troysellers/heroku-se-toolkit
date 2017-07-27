4cc9271c6b0a059b6021ea545f45cda5108bfb4e
tsellers <tsellers@salesforce.com>
2017-07-27 13:15:26 +1000

    fix

--- routes/customers.js
+++ routes/customers.js
@@ -6,7 +6,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-   req.data.title = 'Customer Stories';
+   req.data.title = 'Customers Using Heroku';
    req.data.page = 'customers';
    req.data.customer_style = 'slds-is-active';
 
