37907de549711f5e45d1bc5ae9b4af884f8cdadf
tsellers <tsellers@salesforce.com>
2017-07-28 15:38:34 +1000

    emailer

--- routes/requestSubmit.js
+++ routes/requestSubmit.js
@@ -3,47 +3,45 @@
 var express = require('express');
 var router = express.Router();
 var jsonfile = require('jsonfile');
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var helper = require('sendgrid').mail;
+var request = require('request');
 
 var file = './data/architectures.json';
+
+
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
 
-   var message = 'We have received your request! A member of the Heroku Architect Team will contact you soon.'
-   // if we have Sendgrid.
-   if(process.env.SENDGRID_API_KEY) {
-      console.log(`We have SENDGRID API KEY`);
-      var from_email = new helper.Email(req.body.email);
-      var to_email = new helper.Email(process.env.TO_EMAIL);
-      var subject = 'Request for Heroku Architect from SDO';
-      var content = new helper.Content('text/plain', 'A request for assistance for '+req.body.customer+' has come from an SDO . This is for the '+req.body.region+' region');
-      var mail = new helper.Mail(from_email, subject, to_email, content);
+   console.log('processing the request');
 
-      var request = sg.emptyRequest({
-         method: 'POST',
-         path: '/v3/mail/send',
-         body: mail.toJSON()
-      });
-
-      sg.API(request, (error, response) => {
-         if(error) {
-            console.log(`We have a SENDGRID error ${error}`);
-         } else {
-            console.log(response.statusCode);
-            console.log(response.body);
-            console.log(response.headers);
-         }
-      });
-   } else {
-      console.log(`Failed to post to sendgrid with API Key [${process.env.SENDGRID_API_KEY}]`);
-      message = 'There is no email service configured on this application, your email has not been sent';
-   }
-
-   res.send({
-      message: message
+   var postData = {
+      customer: req.body.customer,
+      contact: req.body.email,
+      region: req.body.region
+   };
+   console.log('postData '+JSON.stringify(postData));
+   
+   request({
+      url: process.env.REQUEST_ENDPOINT,
+      method: 'POST',
+      headers: {
+         'content-type':"application/json",
+      },
+      json: postData
+   }, function(error, resp, body) {
+      var message = "";
+      if (error) {
+         console.log(error);
+         message = 'We have received your request! A member of the Heroku Architect Team will contact you soon.'
+      } else {
+         console.log(body);
+         message = 'We have received your request! A member of the Heroku Architect Team will contact you soon.'
+      } 
+      res.send({
+         message: message
+      }); 
    });
 });
+
 
 module.exports = router;
