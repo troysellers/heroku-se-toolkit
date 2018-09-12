7731e355dfe647b7b814676a9601bd2d2ebadc87
Troy Sellers <tsellers@salesforce.com>
2018-09-12 16:41:11 +1000

    Fix

--- bin/requireHttpsFilter.js
+++ bin/requireHttpsFilter.js
@@ -7,7 +7,7 @@
 let requireHttpsFilter = function (req, res, next) {
 
     if(process.env.MODE != 'dev') {
-        console.log('hostname to test '+req.hostname+ ' = ' +req.hostname.indexOf(process.env.APP_URL));
+        console(`hostname [${req.hostname}] and env URL [${process.env.APP_URL}] and equality [${(req.hostname != process.env.APP_URL)}]`)
         if (req.hostname != process.env.APP_URL) {
             req.data.redirect = 'You have been redirected to the new URL. Please update your bookmarks';
             res.redirect(301, `https://${process.env.APP_URL+req.originalUrl}`); // redirect to the new URL
