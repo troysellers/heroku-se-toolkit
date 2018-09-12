125a89e7f7ea6cc41863d1ad85d38c2638909f2e
Troy Sellers <tsellers@salesforce.com>
2018-09-12 17:57:24 +1000

    add redirect msg

--- bin/requireHttpsFilter.js
+++ bin/requireHttpsFilter.js
@@ -5,12 +5,16 @@
    Have also added in test for old URL, redirect and set notification to user. 
 */
 let requireHttpsFilter = function (req, res, next) {
+    console.log(req.query.redirect);
 
     if(process.env.MODE != 'dev') {
+
         if (req.hostname != process.env.APP_URL || req.protocol != 'https') {
-            req.data.redirect = 'You have been redirected to the new URL. Please update your bookmarks';
-            res.redirect(301, `https://${process.env.APP_URL+req.originalUrl}`); // redirect to the new URL
+            res.redirect(301, `https://${process.env.APP_URL+req.originalUrl}?redirect=true`); // redirect to the new URL
         } else {
+            if(req.query.redirect) {
+                req.data.redirect = `We have updated the URL to ${process.env.APP_URL}. Please update your bookmarks.`;
+            }
             next(); 
         }
     } else {
