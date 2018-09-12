466b75336ed1ce3430a34a81a7d5a705938bcfe1
Troy Sellers <tsellers@salesforce.com>
2018-09-12 15:05:48 +1000

    GA fix

--- bin/requireHttpsFilter.js
+++ bin/requireHttpsFilter.js
@@ -6,12 +6,11 @@
 */
 let requireHttpsFilter = function (req, res, next) {
 
-    req.data.redirect = 'You have been redirected to the new URL. Please update your bookmarks';
-
     if(process.env.MODE != 'dev') {
         if(req.protocl != 'https') {
             res.redirect(301, `https://${req.hostname+req.originalUrl}`); // redirect to the secure version of the URL
         } else if(req.hostname.indexOf(process.env.APP_URL == -1 )) {
+            req.data.redirect = 'You have been redirected to the new URL. Please update your bookmarks';
             res.redirect(301, `https://${process.env.APP_URL+req.originalUrl}`); // redirect to the new URL
         }
     } else {
