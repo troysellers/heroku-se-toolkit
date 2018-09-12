798f35ec9f45db5870c27e26a194c139132df92a
Troy Sellers <tsellers@salesforce.com>
2018-09-12 15:15:03 +1000

    redirect fix

--- bin/requireHttpsFilter.js
+++ bin/requireHttpsFilter.js
@@ -7,9 +7,7 @@
 let requireHttpsFilter = function (req, res, next) {
 
     if(process.env.MODE != 'dev') {
-        if(req.protocl != 'https') {
-            res.redirect(301, `https://${req.hostname+req.originalUrl}`); // redirect to the secure version of the URL
-        } else if(req.hostname.indexOf(process.env.APP_URL == -1 )) {
+        if(req.protocl != 'https' || req.hostname.indexOf(process.env.APP_URL == -1 )) {
             req.data.redirect = 'You have been redirected to the new URL. Please update your bookmarks';
             res.redirect(301, `https://${process.env.APP_URL+req.originalUrl}`); // redirect to the new URL
         }
